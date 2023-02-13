ecdc1b343e47e515b90faad5a94ecd00c9c94b1b
XxLuisFer15xX <luisfer.sm15@gmail.com>
2023-02-13 16:33:51 -0600

    :memo: Update and fixing swagger documentation

--- src/index.js
+++ src/index.js
@@ -46,10 +46,17 @@ app.use(
   swaggerUI.setup(undefined, swaggerOptions),
 )
 app.get('/', (_req, res) => {
-  const html = `<h1><a href="${SERVER_URL_NAME}/api-docs">Swagger api documentation</a></h1>`
+  const html = `<h1><a href="${process.env.SERVER_URL_NAME}/api-docs">Swagger api documentation</a></h1>`
   return res.status(200).send(html)
 })
-app.get('/api/v1/docs/swagger.yaml', (_req, res) => res.json(swaggerDocumentV1))
+app.get('/api/v1/docs/swagger.yaml', (_req, res) => {
+  swaggerDocumentV1.servers = [
+    {
+      url: `${process.env.SERVER_URL_NAME}/api/v1`,
+    },
+  ]
+  res.json(swaggerDocumentV1)
+})
 app.use('/api/v1', authRoutes)
 app.use(`/api/v1/${Paths.users}`, usersRoutes)
 app.use(`/api/v1/${Paths.permissions}`, permissionsRoutes)
