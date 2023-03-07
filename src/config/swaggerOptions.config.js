f869dec6084e2bd380428c67b6cbb61f4b87451a
JOVM99 <jvieramejia@gmail.com>
2023-03-07 15:59:27 -0600

    :bug: correction swagger options

--- src/config/swaggerOptions.config.js
+++ src/config/swaggerOptions.config.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
   explorer: true,
   swaggerOptions: {
     urls: [
       {
-        url: `${'http://localhost:3000'}/api/v1/docs/swagger.yaml`,
+        url: `${process.env.SERVER_URL_NAME}/api/v1/docs/swagger.yaml`,
         name: 'v1',
       },
     ],
